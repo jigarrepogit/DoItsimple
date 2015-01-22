d210227fe3011b98a7a3fe41f888df7bec55fb2f
jigarrepogit <jigartester1@yahoo.com>
2015-01-22 14:37:39 -0600

    final checkin

---
+++ AmericanWordsTest/AmericanWordsTest/CharToNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmericanWordsTest
+{
+    public static class CharToNumber
+    {
+        public static int ConvertCharToNumber(this char text)
+        {
+            return (text - 'a') + 1;
+        }
+    }
+}
--- AmericanWordsTest/AmericanWordsTest/Program.cs
+++ AmericanWordsTest/AmericanWordsTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace AmericanWordsTest
     {
         static void Main(string[] args)
         {
+            string[] words = File.ReadLines(@"..\..\File\american-words.txt").ToArray();
+            var intsum = 0;
+            foreach (string value in words)
+            {
+                intsum = 0;
+                foreach (char c in value)
+                {
+                    intsum += (c == ' ' || c == '\'' ? 0 : c.ConvertCharToNumber());
+                    //Console.WriteLine("char ->" + c + "number " + c.TextToNumber().ToString());
+                }
+                //Console.WriteLine(value + " ->" +intsum.ToString());
+                if (intsum == 100)
+                    Console.WriteLine("100 words -> " + value);
+            }
         }
     }
 }
